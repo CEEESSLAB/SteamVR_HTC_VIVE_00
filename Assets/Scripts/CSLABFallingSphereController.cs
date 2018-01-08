9eaec354f4ea1ba807d71d0a10918e99f2c0dba0
Sensor Lab <sensor.lab@concordia.ca>
2018-01-07 22:22:11 -0500

    Completed SteamVR Gaze Scene

---
+++ Assets/Scripts/CSLABFallingSphereController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSLABFallingSphereController : CSLABBaseInteractableObject
+{
+
+
+    private bool initiatedFalling = false;
+    public Transform FallSphere;
+    public Renderer objRenderer;
+    protected Color originalColor;
+    protected Material objectMaterial;
+    public Color downColor;
+    // Use this for initialization
+    void Awake()
+    {
+        objectMaterial = objRenderer.material;
+        originalColor = objectMaterial.color;
+    }
+
+    public override void RayStay(RaycastHit hit, CSLABControllerInput controllerRef = null)
+    {
+        objectMaterial.color = downColor;
+
+
+        if (controllerRef != null)
+        {
+            if (!initiatedFalling)
+            {
+                initiatedFalling = true;
+                //public void InvokeRepeating(string methodName, float time, float repeatRate); 
+                InvokeRepeating("RepeatingFunction", .2f, .5f);
+
+
+            }
+        }
+    }
+
+    void RepeatingFunction()
+    {
+        float x = Random.Range(transform.position.x - 1, transform.position.x + 1);
+        float z = Random.Range(transform.position.z - 1, transform.position.z + 1);
+        float y = 10f;
+        Vector3 newPosition = new Vector3(x, y, z);
+
+        Instantiate(FallSphere, newPosition, Quaternion.identity);
+
+    }
+
+    public override void RayExit(CSLABControllerInput controllerRef = null)
+    {
+        initiatedFalling = false;
+        objectMaterial.color = originalColor;
+        CancelInvoke();
+    }
+}
---
+++ Assets/Scripts/CSLABPointController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class CSLABPointController : MonoBehaviour {
+    protected CSLABControllerInput controllerRef;
+    public GameObject prefabBullet;
+    public float bulletSpeed = 2f;
+    private void Awake()
+    {
+        controllerRef = GetComponent<CSLABControllerInput>();
+    }
+
+     void Update()
+    {
+        if (controllerRef.controller.GetPressDown(EVRButtonId.k_EButton_SteamVR_Trigger))
+        {
+            Debug.Log("Trigger");
+            ShootBullet();
+
+        }
+    }
+
+    private void ShootBullet()
+    {
+        GameObject bullet = Instantiate(prefabBullet);
+        bullet.transform.position = controllerRef.gameObject.transform.position;
+        bullet.transform.rotation = controllerRef.gameObject.transform.rotation;
+        Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+        bulletRb.AddForce(transform.forward * bulletSpeed);
+
+
+    }
+    
+
+
+}
